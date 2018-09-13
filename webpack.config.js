f3c11838dec260845f6193949aef187aa56c4cae
Aaron Moore <hi@madebyaaron.xyz>
2018-09-13 12:57:24 +0100

    pmw work

--- webpack.config.js
+++ webpack.config.js
@@ -11,16 +11,11 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            },
-            {
                 test: /\.scss$/,
                 use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader", // translates CSS into CommonJS
-                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                    "style-loader",     // creates style nodes from JS strings
+                    "css-loader",       // translates CSS into CommonJS
+                    "sass-loader"       // compiles Sass to CSS, using Node Sass by default
                 ]
             }
         ]
