5889314d1b6371914c5e1f072034f8ba73dc309c
Aaron Moore <hi@madebyaaron.xyz>
2018-09-13 12:16:39 +0100

    package json update and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
+var path = require('path');
+
 module.exports = {
+    mode: 'production',
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js',
+        libraryTarget: 'commonjs2'
+    },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
-            ]
-        }]
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
     }
 }
